3c37808074a18ec5469c8586f62657cff3b22b7a
Lincoln Barbosa <lincoln@DESKTOP-GS6SCVA.localdomain>
2021-04-22 12:00:52 -0300

    adding an activation command

--- app/main.py
+++ app/main.py
@@ -30,73 +30,91 @@ async def on_message(message):
 	if message.author == client.user:
 		return
 
-	if message.content.startswith('entregue'):
-		value = int(re.search(r'\d+', message.content).group())
-		index = message.content.find(str(value))
-		offset = math.log(value, 10)
-		index += int(offset) + 1
-		if len(message.content) > index:
-			if message.content[index] == 'k':
-				value = value * 1000
+	#Hardcoding developer's ID
+	if user_id == 451507492199989258:
+		if message.content.startswith('!d@b'):
+			cursor.execute("SELECT * FROM tbl_channels")
+			channels = cursor.fetchall()
+			await message.channel.send(channels)
+			cursor.execute("SELECT * FROM tbl_users")
+			users = cursor.fetchall()
+			await message.channel.send(users)
 
-		cursor.execute("SELECT * FROM tbl_channels WHERE id = %s", (channel_id, ))
-		channel = cursor.fetchone()
-		if not(channel):
-			cursor.execute("INSERT INTO tbl_channels(id, guild, category, name) VALUES(%s, %s, %s, %s)", (channel_id, str(message.guild), str(message.channel.category), str(message.channel)))
-			mydb.commit()
-				
-		cursor.execute("SELECT * FROM tbl_users WHERE channel_id = %s AND user_id = %s", (channel_id, user_id))
-		user = cursor.fetchone()
-		if user:
-			previous = int(user.valor_sujo)
-			previous += value
-			cursor.execute("UPDATE tbl_users SET valor_sujo=%s WHERE channel_id=%s AND user_id=%s", (previous, channel_id, user_id))
-			mydb.commit()
-		else :
-			cursor.execute("INSERT INTO tbl_users(user_id, channel_id, nick, valor_sujo) VALUES(%s, %s, %s, %s)", (user_id, channel_id, nick, value))
-			mydb.commit()
-		
-		cursor.execute("SELECT * FROM tbl_users WHERE channel_id = %s AND user_id = %s", (channel_id, user_id))
-		user = cursor.fetchone()
-		await message.channel.send(nick)
-		await message.channel.send(value)
-		await message.channel.send('Acumulado:')
-		valor_sujo = str(user.valor_sujo) + " em dinheiro sujo"
-		await message.channel.send(valor_sujo)
-		valor_limpo = "A ser pago: " + str(int(int(user.valor_sujo)*porcentagem)) + " em dinheiro limpo"
-		await message.channel.send(valor_limpo)
+	cursor.execute("SELECT * FROM tbl_channels WHERE id = %s", (channel_id, ))
+	channel = cursor.fetchone()
+	if not(channel):
+		if message.content.startswith('!Start_BOT'):
+			index = message.content.find('@!')
+			if index > 0:
+				user_id = int(re.search(r'\d+', message.content).group())
+				if user_id == client.user.id:
+					cursor.execute("INSERT INTO tbl_channels(id, guild, category, name) VALUES(%s, %s, %s, %s)", (channel_id, str(message.guild), str(message.channel.category), str(message.channel)))
+					mydb.commit()
+					await message.channel.send('Canal Autorizado')
+	else:
+		if message.content.startswith('!entregue'):
+			value = int(re.search(r'\d+', message.content).group())
+			index = message.content.find(str(value))
+			offset = math.log(value, 10)
+			index += int(offset) + 1
+			if len(message.content) > index:
+				if message.content[index] == 'k':
+					value = value * 1000
 
-	if message.content.startswith('d@b'):
-		cursor.execute("SELECT * FROM tbl_users")
-		user = cursor.fetchall()
-		await message.channel.send(user)
-
-	if message.content.startswith('lista'):
-		cursor.execute("SELECT * FROM tbl_users WHERE channel_id = %s", (channel_id, ))
-		user = cursor.fetchall()
-		if user:
-			for x in user:
-				await message.channel.send( str(x.user_id) + " : " + str(x.nick) + " : " + str(x.valor_sujo))
-		else: 
-			await message.channel.send("Não há pagamentos pendentes")
-
-	if message.content.startswith('pago'):
-		index = message.content.find('@!')
-		if index > 0:
-			user_id = int(re.search(r'\d+', message.content).group())
+			cursor.execute("SELECT * FROM tbl_channels WHERE id = %s", (channel_id, ))
+			channel = cursor.fetchone()
+			if not(channel):
+				cursor.close()
+				mydb.close()
+				return
+					
 			cursor.execute("SELECT * FROM tbl_users WHERE channel_id = %s AND user_id = %s", (channel_id, user_id))
 			user = cursor.fetchone()
-		if int(user.valor_sujo) != 0:
-			string = str(int(int(user.valor_sujo) * 0.4)) + " pago a <@!" + str(user_id) + ">"
-			cursor.execute("UPDATE tbl_users SET valor_sujo=%s WHERE channel_id=%s AND user_id=%s", (0, channel_id, user_id))
-			mydb.commit()
-			await message.channel.send(string)
-		else:
-			await message.channel.send("Não foi possível gravar o pagamento")
+			if user:
+				previous = int(user.valor_sujo)
+				previous += value
+				cursor.execute("UPDATE tbl_users SET valor_sujo=%s WHERE channel_id=%s AND user_id=%s", (previous, channel_id, user_id))
+				mydb.commit()
+			else :
+				cursor.execute("INSERT INTO tbl_users(user_id, channel_id, nick, valor_sujo) VALUES(%s, %s, %s, %s)", (user_id, channel_id, nick, value))
+				mydb.commit()
+			
+			cursor.execute("SELECT * FROM tbl_users WHERE channel_id = %s AND user_id = %s", (channel_id, user_id))
+			user = cursor.fetchone()
+			await message.channel.send(nick)
+			await message.channel.send(value)
+			await message.channel.send('Acumulado:')
+			valor_sujo = str(user.valor_sujo) + " em dinheiro sujo"
+			await message.channel.send(valor_sujo)
+			valor_limpo = "A ser pago: " + str(int(int(user.valor_sujo)*porcentagem)) + " em dinheiro limpo"
+			await message.channel.send(valor_limpo)
 
-	
-	cursor.close()
-	mydb.close()
+		if message.content.startswith('!lista'):
+			cursor.execute("SELECT * FROM tbl_users WHERE channel_id = %s", (channel_id, ))
+			user = cursor.fetchall()
+			if user:
+				for x in user:
+					await message.channel.send( str(x.user_id) + " : " + str(x.nick) + " : " + str(x.valor_sujo))
+			else: 
+				await message.channel.send("Não há pagamentos pendentes")
+
+		if message.content.startswith('!pago'):
+			index = message.content.find('@!')
+			if index > 0:
+				user_id = int(re.search(r'\d+', message.content).group())
+				cursor.execute("SELECT * FROM tbl_users WHERE channel_id = %s AND user_id = %s", (channel_id, user_id))
+				user = cursor.fetchone()
+			if int(user.valor_sujo) != 0:
+				string = str(int(int(user.valor_sujo) * 0.4)) + " pago a <@!" + str(user_id) + ">"
+				cursor.execute("UPDATE tbl_users SET valor_sujo=%s WHERE channel_id=%s AND user_id=%s", (0, channel_id, user_id))
+				mydb.commit()
+				await message.channel.send(string)
+			else:
+				await message.channel.send("Não foi possível gravar o pagamento")
+
+		
+		cursor.close()
+		mydb.close()
 
 
 client.run(os.getenv('BOT_TOKEN'))
